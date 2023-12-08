2dd241993479e1aad7c55fcaa39bdd335ae9f378
Сыймык Джумагулов <jumagulovsyimyk0404@gmail.com>
2023-12-08 11:09:20 +0600

    start adaptive

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import NavScrollExample from "./components/Navbar";
 import MainRoutes from "./routes/MainRoutes";
 import Footer from "./components/Footer";
+import "./components/adaptive/Adaptive.css";
 
 const App = () => {
   return (
